99497df8426a751e4f82bfa13051a3ef49cd6c70
Mengxuan <xiamx2004@gmail.com>
2017-05-26 17:00:38 -0400

    removed placeholder test code

--- test/expostal_test.exs
+++ test/expostal_test.exs
@@ -2,10 +2,6 @@ defmodule ExpostalTest do
   use ExUnit.Case
   doctest Expostal
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-
   test "parse address" do
     address = "615 Rene Levesque Ouest, Montreal, QC, Canada"
     parsed = Expostal.parse_address(address)
