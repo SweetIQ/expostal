1d1526ecb7fef958ac5aea7339d3668968af1bbf
Mengxuan Xia <xiamx2004@gmail.com>
2017-07-06 12:32:23 -0400

    v0.1.5

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Expostal.Mixfile do
 
   def project do
     [app: :expostal,
-     version: "0.1.4",
+     version: "0.1.5",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
